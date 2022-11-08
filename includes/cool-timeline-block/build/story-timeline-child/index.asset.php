8bf99e211b7d560418c25da5c9bd6c295c830c2f
Raphaël Droz <raphael.droz@gmail.com>
2022-11-08 09:36:19 -0300

    fix builds

--- includes/cool-timeline-block/build/story-timeline-child/index.asset.php
+++ includes/cool-timeline-block/build/story-timeline-child/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'f3e4bdaef495816091b3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '074a759f856934420647');
--- includes/cool-timeline-block/build/story-timeline/index.asset.php
+++ includes/cool-timeline-block/build/story-timeline/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-element', 'wp-i18n'), 'version' => 'b2a7b9dd8c36247086a7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-element', 'wp-i18n'), 'version' => 'cdb5e0d2c47ce11bb107');
