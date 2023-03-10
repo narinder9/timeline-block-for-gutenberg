c2df9b3336d3bb33238999c74103e699d0f6a3a0
Robin Schülein <robins@animalequality.de>
2023-03-10 16:37:32 +0100

    update build

--- includes/cool-timeline-block/build/story-timeline-child/index.asset.php
+++ includes/cool-timeline-block/build/story-timeline-child/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '1b29546e59e3de5f74af');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '2b8dbebb9fc51d0f5820');
--- includes/cool-timeline-block/build/story-timeline/index.asset.php
+++ includes/cool-timeline-block/build/story-timeline/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5aaf7be5e6178d1e1ebf');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cab249fefb031a14b749');
