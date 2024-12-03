ff0fc3fba566d31ce95b56b4c517afdf5e6676c3
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2024-12-03 13:25:52 +0530

    Update webpack config file.
    1. Update webpack config file.

--- includes/cool-timeline-block/webpack.config.js
+++ includes/cool-timeline-block/webpack.config.js
@@ -1,8 +1,12 @@
 let parse = await import('@wordpress/scripts/config/webpack.config.js'), defaultConfig = parse.default;
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-if (process.env.TARGET == 'node') {
-  defaultConfig.output.globalObject = 'global';
-  defaultConfig.output.publicPath = '';
-}
+const filePath = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(filePath);
+
+// Save each index file in the dist folder starting from block.index
+defaultConfig.output.filename = 'block.build.js'; // Use a placeholder for dynamic naming
+defaultConfig.output.path = path.resolve(__dirname, 'dist');
 
 export default defaultConfig;
