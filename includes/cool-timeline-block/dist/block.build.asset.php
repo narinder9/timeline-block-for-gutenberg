a788e7a341449f68d278eb40746da69c274fb299
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2024-12-03 15:07:19 +0530

    Added wordpress control new margin-free styles.
    1. Added wordpress 6.7 new margin-free styles to fixed JS deprecated message in SelectControl & ToggleControl.

--- includes/cool-timeline-block/dist/block.build.asset.php
+++ includes/cool-timeline-block/dist/block.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7a52df7ee5f6c1f27f3b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cb925b5c3a2d73f31e8f');
